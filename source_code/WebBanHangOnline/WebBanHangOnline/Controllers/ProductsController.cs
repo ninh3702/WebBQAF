9d39a22aa597dc1c1c445948c9360a9b4252bc58
ninh3702 <ninhpro96@gmail.com>
2023-12-14 21:43:34 +0700

    BQAF

---
+++ source_code/WebBanHangOnline/WebBanHangOnline/Controllers/ProductsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+
+namespace WebBanHangOnline.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Products
+        public ActionResult Index()
+        {
+            var items = db.Products.ToList();
+            
+            return View(items);
+        }
+
+        public ActionResult Detail(string alias,int id)
+        {
+            var item = db.Products.Find(id);
+            if (item != null)
+            {
+                db.Products.Attach(item);
+                item.ViewCount = item.ViewCount + 1;
+                db.Entry(item).Property(x => x.ViewCount).IsModified = true;
+                db.SaveChanges();
+            }
+            
+            return View(item);
+        }
+        public ActionResult ProductCategory(string alias,int id)
+        {
+            var items = db.Products.ToList();
+            if (id > 0)
+            {
+                items = items.Where(x => x.ProductCategoryId == id).ToList();
+            }
+            var cate = db.ProductCategories.Find(id);
+            if (cate != null)
+            {
+                ViewBag.CateName = cate.Title;
+            }
+
+            ViewBag.CateId = id;
+            return View(items);
+        }
+
+        public ActionResult Partial_ItemsByCateId()
+        {
+            var items = db.Products.Where(x => x.IsHome && x.IsActive).Take(12).ToList();
+            return PartialView(items);
+        }
+
+        public ActionResult Partial_ProductSales()
+        {
+            var items = db.Products.Where(x => x.IsSale && x.IsActive).Take(12).ToList();
+            return PartialView(items);
+        }
+        [HttpPost]
+        public ActionResult DecreaseProductQuantity(int id)
+        {
+            // Load product from database
+            var product = db.Products.Find(id);
+
+            // Decrease quantity
+            if (product.Quantity > 0)
+            {
+                product.Quantity -= 1;
+                db.SaveChanges();
+
+                return Json(new { success = true });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Product is out of stock" });
+            }
+        }
+    }
+}
---
+++ source_code/WebBanHangOnline/WebBanHangOnline/Models/EditViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace WebBanHangOnline.Models.ViewModels
+{
+    public class EditViewModel
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "User name is required")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        [Display(Name = "User name")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
